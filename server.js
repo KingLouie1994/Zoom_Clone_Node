1db001252a2f2378b20dd68c9c2491ead9e36b58
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-02-17 16:08:55 +0100

    creating rooms with uuid dependency

--- server.js
+++ server.js
@@ -4,12 +4,19 @@ const express = require("express");
 // Initialising the application invoking express
 const server = express();
 
+// Imports of third party libraries
+const { v4: uuidv4 } = require('uuid');
+
 // Set ejs as view engine
 server.set("view engine", "ejs");
 
 // Routes
 server.get("/", (req, res, next) => {
-  res.render('room')
+  res.redirect(`/${uuidv4()}`);
+});
+
+server.get("/:room", (req, res, next) => {
+  res.render("room", { roomId: req.params.room });
 });
 
 server.listen(4000);
